1fb2fbf5ad79ff2f7147adb8214ff8ab413a4c0d
Andrea Mannocci <andremann@libero.it>
2023-05-16 16:07:43 +0200

    installing cookiecutter data science

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,19 +1,69 @@
 # -*- coding: utf-8 -*-
+import os
+from urllib.request import urlretrieve
+from urllib.parse import urlsplit
+import zipfile
 import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
+logger = logging.getLogger(__name__)
+
+HORIZON_URL = 'https://cordis.europa.eu/data/cordis-HORIZONprojects-csv.zip'
+H2020_URL = 'http://cordis.europa.eu/data/cordis-h2020projects-csv.zip'
+FP7_URL = 'http://cordis.europa.eu/data/cordis-fp7projects-csv.zip'
+CSV_FILES = ['https://cordis.europa.eu/data/FP6/cordis-fp6projects.csv',
+             'https://cordis.europa.eu/data/FP6/cordis-fp6organizations.csv',
+             'https://cordis.europa.eu/data/FP5/cordis-fp5organizations.csv',
+             'https://cordis.europa.eu/data/FP5/cordis-fp5projects.csv',
+             'https://cordis.europa.eu/data/FP4/cordis-fp4organizations.csv',
+             'https://cordis.europa.eu/data/FP4/cordis-fp4projects.csv',
+             'https://cordis.europa.eu/data/FP3/cordis-fp3organizations.csv',
+             'https://cordis.europa.eu/data/FP3/cordis-fp3projects.csv',
+             'https://cordis.europa.eu/data/FP2/cordis-fp2projects.csv',
+             'https://cordis.europa.eu/data/FP2/cordis-fp2organizations.csv',
+             'https://cordis.europa.eu/data/FP1/cordis-fp1organizations.csv',
+             'https://cordis.europa.eu/data/FP1/cordis-fp1projects.csv']
+
+
+def download_zip(url, path):
+    zip_name = urlsplit(url).path.split('/')[-1]
+    unzipped_folder = zip_name.split('.')[0]
+    zip_path = os.path.join(path, zip_name)
+    unzipped_path = os.path.join(path, unzipped_folder)
+    if not os.path.exists(unzipped_path):
+        if not os.path.exists(zip_path):
+            logger.info('downloading %s' % url)
+            urlretrieve(url, zip_path)
+
+        logger.info('unzipping zip_name')
+        zip_ref = zipfile.ZipFile(zip_path, 'r')
+        zip_ref.extractall(unzipped_path)
+        zip_ref.close()
+
+        logger.info('cleaning')
+        os.remove(zip_path)
+
+
+def download_csv(urls, path):
+    for url in urls:
+        csv_name = urlsplit(url).path.split('/')[-1]
+        csv_path = os.path.join(path, csv_name)
+        if not os.path.exists(csv_path):
+            logger.info('downloading %s' % csv_name)
+            urlretrieve(url, csv_path)
+
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+@click.argument('output_filepath', type=click.Path(exists=True))
+def main(output_filepath):
+    """ Downloads data into ../raw.
     """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    download_zip(HORIZON_URL, output_filepath)
+    download_zip(H2020_URL, output_filepath)
+    download_zip(FP7_URL, output_filepath)
+    download_csv(CSV_FILES, output_filepath)
 
 
 if __name__ == '__main__':
